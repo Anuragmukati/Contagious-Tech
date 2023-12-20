c101a14f4f6c6aa4a25a37a67f53d888b92b05f3
Anuragmukati <anuragmukati09@gmail.com>
2023-12-20 10:49:02 +0530

    Revert "some additional"
    This reverts commit c14aae05957b3c6228a7e5893e21a0e7cce4204b.

--- clip_object_tracker.py
+++ clip_object_tracker.py
@@ -81,7 +81,7 @@ def update_tracks(tracker, frame_count, save_txt, txt_path, save_img, view_img, 
             with open(txt_path + '.txt', 'a') as f:
                 f.write('frame: {}; track: {}; class: {}; bbox: {};\n'.format(frame_count, track.track_id, class_num,
                                                                               *xywh))
-        #id var declare
+#hard var declare
         
         id = track.track_id
         try:
